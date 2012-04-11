46b17aeeda56c57dd17fa0e0b44cceb597ce6233
asofold <asofold@web.de>
2012-04-11 21:06:20 +0200

    + removeAllHooks

---
+++ SimplyVanish/src/asofold/simplyvanish/util/HookUtil.java
@@ -0,0 +1,149 @@
+package asofold.simplyvanish.util;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+
+import asofold.simplyvanish.hooks.Hook;
+import asofold.simplyvanish.hooks.HookListener;
+import asofold.simplyvanish.hooks.HookSupport;
+
+/**
+ * Auxiliary methods for hooks.
+ * @author mc_dev
+ *
+ */
+public class HookUtil {
+	/**
+	 * Hooks by purpose.
+	 */
+	private final Map<HookSupport, List<Hook>> usedHooks = new HashMap<HookSupport, List<Hook>>();
+	
+	private final Map<String, HookListener> usedHookListeners = new HashMap<String, HookListener>();
+	
+	/**
+	 * Registered hooks by name.
+	 */
+	private final Map<String, Hook> registeredHooks = new HashMap<String, Hook>();
+	
+	
+	public boolean addHook(Hook hook) {
+		boolean existed = removeHook(hook);
+		try{
+			String hookName = hook.getHookName();
+			// add hook !
+			registeredHooks.put(hookName, hook);
+			HookSupport[] supported = hook.getSupportedMethods();
+			if (supported == null) supported = HookSupport.values();
+			boolean hasListener = false;
+			for (HookSupport sup : supported){
+				getUsedHooks(sup).add(hook);
+				if (sup == HookSupport.LISTENER) hasListener = true;
+			}
+			if (hasListener){
+				HookListener listener = hook.getListener();
+				if (listener != null){
+					PluginManager pm = Bukkit.getServer().getPluginManager();
+					pm.registerEvents(listener, pm.getPlugin("SimplyVanish"));
+					usedHookListeners.put(hookName, listener);
+				}
+			}
+		} catch (Throwable t){
+			Utils.warn("Disable hook ("+hook.getHookName()+") due to failure on registration: "+t.getMessage());
+			t.printStackTrace();
+			removeHook(hook);
+		}
+		return existed;
+	}
+	
+	public boolean removeHook(Hook hook) {
+		// TODO maybe also check for the hook itself.
+		return removeHook(hook.getHookName());
+	}
+	
+	public boolean removeHook(String hookName) {
+		Hook hook = registeredHooks.remove(hookName);
+		if (hook == null) return false;
+		HookListener listener = usedHookListeners.remove(hookName);
+		if (listener != null){
+			try{
+				if (!listener.unregisterEvents()) Utils.warn("HookListener ("+hookName+") returns failure on unregister.");
+			} catch (Throwable t){
+				Utils.warn("Failed to unregister HookListener ("+hookName+"): "+t.getMessage());
+				t.printStackTrace();
+			}
+		}
+		for (HookSupport sup : usedHooks.keySet()){
+			List<Hook> rem = new LinkedList<Hook>();
+			List<Hook> present = getUsedHooks(sup);
+			for (Hook ref : present){
+				if (ref==hook || ref.getHookName().equals(hookName)) rem.add(ref); // equals unnecessary ?
+			}
+			present.removeAll(rem);
+		}
+		return true;
+	}
+	
+	/**
+	 * (Over cautious.)
+	 * @param purpose
+	 * @return
+	 */
+	public List<Hook> getUsedHooks(HookSupport purpose){
+		
+		
+		List<Hook> hooks = null;
+		if (purpose != null) hooks = usedHooks.get(purpose);
+		if (hooks == null) return new LinkedList<Hook>();
+		return hooks;
+	}
+
+	public final void callAfterVanish(String playerName) {
+		HookSupport sup = HookSupport.AFTER_VANISH;
+		for (Hook hook : getUsedHooks(sup)){
+			try{
+				
+			} catch (Throwable t){
+				onHookCallError(sup, hook, playerName, t);
+			}
+		}
+	}
+
+	public boolean callBeforeVanish(String playerName) {
+		boolean res = true;
+		HookSupport sup = HookSupport.AFTER_VANISH;
+		for (Hook hook : getUsedHooks(sup)){
+			try{
+				if (!hook.beforeVanish(playerName)) res = false;
+			} catch (Throwable t){
+				onHookCallError(sup, hook, playerName, t);
+			}
+		}
+		return res;
+	}
+	
+	public void onHookCallError(HookSupport sup, Hook hook, String playerName, Throwable t) {
+		String msg;
+		if (t==null) msg = "<unknown>";
+		else msg = t.getMessage();
+		Utils.warn("Error on calling "+sup+" on hook("+hook.getHookName()+") for player "+playerName+": "+msg);
+		if (t!= null) t.printStackTrace();
+	}
+	
+	public void removeAllHooks(){
+		List<String> names = new LinkedList<String>();
+		names.addAll(registeredHooks.keySet());
+		for ( String name : names){
+			removeHook(name);
+			// TODO: maybe something more complete.
+		}
+		// safety: 
+		usedHookListeners.clear();
+		usedHooks.clear();
+		registeredHooks.clear();
+	}
+}
