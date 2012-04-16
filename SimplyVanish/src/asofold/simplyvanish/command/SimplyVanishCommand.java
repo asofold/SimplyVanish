3fcaf686215daba332d1ff94f93d4f50c0b635e9
asofold <asofold@web.de>
2012-04-16 10:11:12 +0200

    add /sv flags + restructure commands

--- SimplyVanish/src/asofold/simplyvanish/command/SimplyVanishCommand.java
+++ SimplyVanish/src/asofold/simplyvanish/command/SimplyVanishCommand.java
@@ -13,7 +13,9 @@ import org.bukkit.entity.Player;
 
 import asofold.simplyvanish.SimplyVanish;
 import asofold.simplyvanish.SimplyVanishCore;
+import asofold.simplyvanish.config.Flag;
 import asofold.simplyvanish.config.Path;
+import asofold.simplyvanish.config.VanishConfig;
 import asofold.simplyvanish.config.compatlayer.CompatConfig;
 import asofold.simplyvanish.util.Utils;
 
@@ -104,7 +106,7 @@ public class SimplyVanishCommand{
 
 	public boolean onCommand(CommandSender sender, Command command, String label,
 			String[] args) {
-		SimplyVanish plugin = core.getPlugin();
+//		SimplyVanish plugin = core.getPlugin();
 		label = getMappedCommandLabel(label);
 		int len = args.length;
 		boolean hasFlags = false;
@@ -115,41 +117,8 @@ public class SimplyVanishCommand{
 			} 
 			else break;
 		}
-		
-		if ( label.equals("vanish") && len==0 ){
-			if ( !Utils.checkPlayer(sender)) return true;
-			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.self")) return true;
-			// Make sure the player is vanished...
-			if (hasFlags) core.setFlags(((Player) sender).getName(), args, len, sender, false, false, false);
-			if (!SimplyVanish.setVanished((Player) sender, true)) Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
-			return true;
-		} 
-		else if ( label.equals("vanish") && len==1 ){
-			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.other")) return true;
-			// Make sure the other player is vanished...
-			String name = args[0].trim();
-			if (hasFlags) core.setFlags(name, args, len, sender, false, true, false);
-			if (SimplyVanish.setVanished(name, true)) Utils.send(sender, SimplyVanish.msgLabel + "Vanish player: "+name);
-			else Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
-			return true;
-		} 
-		else if (label.equals("reappear") && len==0 ){
-			if ( !Utils.checkPlayer(sender)) return true;
-			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.self") && !Utils.checkPerm(sender, "simplyvanish.reappear.self")) return true;
-			// Let the player be seen...
-			if (hasFlags) core.setFlags(((Player) sender).getName(), args, len, sender, false, false, false);
-			if (!SimplyVanish.setVanished((Player) sender, false)) Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
-			return true;
-		} 
-		else if ( label.equals("reappear") && len==1 ){
-			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.other") && !Utils.checkPerm(sender, "simplyvanish.reappear.other")) return true;
-			// Make sure the other player is shown...
-			String name = args[0].trim();
-			if (hasFlags) core.setFlags(name, args, len, sender, false, true, false);
-			if (SimplyVanish.setVanished(name, false)) Utils.send(sender, SimplyVanish.msgLabel + "Show player: "+name);
-			else Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
-			return true;
-		} 
+		if ( label.equals("vanish")) return vanishCommand(sender, args, len, hasFlags);
+		else if (label .equals("reappear")) return reappearCommand(sender, args, len, hasFlags);
 		else if ( label.equals("tvanish") && len==0 ){
 			if ( !Utils.checkPlayer(sender)) return true;
 			Player player = (Player) sender;
@@ -164,57 +133,140 @@ public class SimplyVanishCommand{
 			return true;
 		} 
 		else if ( label.equals("simplyvanish") || label.equals("vanflag")){
-			
-			if (label.equals("simplyvanish")){
-				if (!hasFlags && len==1 && args[0].equalsIgnoreCase("reload")){
-					if ( !Utils.checkPerm(sender, "simplyvanish.reload")) return true;
-					plugin.loadSettings();
-					Utils.send(sender, SimplyVanish.msgLabel + ChatColor.YELLOW+"Settings reloaded.");
-					return true;
-				}
-				else if (!hasFlags && len==1 && args[0].equalsIgnoreCase("drop")){
-					if ( !Utils.checkPerm(sender, "simplyvanish.cmd.drop")) return true;
-					if (!Utils.checkPlayer(sender)) return true;
-					Utils.dropItemInHand((Player) sender);
-					return true;
-				}
-				else if (len==1 && args[0].equals(SimplyVanish.cmdNoOpArg)) return true;
-				else if (len==1 && args[0].equalsIgnoreCase("stats")){
-					if ( !Utils.checkPerm(sender, "simplyvanish.stats.display")) return true;
-					Utils.send(sender, SimplyVanish.stats.getStatsStr(true));
-					return true;
-				} 
-				else if (len==2 && args[0].equalsIgnoreCase("stats") && args[1].equalsIgnoreCase("reset")){
-					if ( !Utils.checkPerm(sender, "simplyvanish.stats.reset")) return true;
-					SimplyVanish.stats.clear();
-					Utils.send(sender, SimplyVanish.msgLabel+"Stats reset.");
-					return true;
-				}
-			}
-			
-			if (hasFlags && len == 0){
-				if (!Utils.checkPlayer(sender)) return true;
-				core.setFlags(((Player)sender).getName(), args, len, sender, false, false, true);
-				if (Utils.hasPermission(sender, "simplyvanish.flags.display.self")) core.onShowFlags((Player) sender, null);
-				return true;
-			} 
-			else if (len == 0){
-				if (!Utils.checkPlayer(sender)) return true;
-				if (Utils.hasPermission(sender, "simplyvanish.flags.display.self")) core.onShowFlags((Player) sender, null);
-				else sender.sendMessage(SimplyVanish.msgLabel+ChatColor.RED+"You do not have permission to display flags.");
-				return true;
-			} 
-			else if (hasFlags && len==1){
-				core.setFlags(args[0], args, len, sender, false, true, true);
-				if (Utils.hasPermission(sender, "simplyvanish.flags.display.other")) core.onShowFlags(sender, args[0]);
-				return true;
-			}
-			else if (len==1){
-				if (Utils.hasPermission(sender, "simplyvanish.flags.display.other")) core.onShowFlags(sender, args[0]);
-				else sender.sendMessage(SimplyVanish.msgLabel+ChatColor.RED+"You do not have permission to display flags of others.");
-				return true;
-			}
+			if (label.equals("simplyvanish") && !hasFlags) return rootCommand(sender, args);
+			else return flagCommand(sender, args, len, hasFlags);
 		}
+		return unrecognized(sender);
+	}
+
+	private boolean flagCommand(CommandSender sender, String[] args, int len,
+			boolean hasFlags) {
+		if (hasFlags && len == 0){
+			if (!Utils.checkPlayer(sender)) return true;
+			core.setFlags(((Player)sender).getName(), args, len, sender, false, false, true);
+			if (Utils.hasPermission(sender, "simplyvanish.flags.display.self")) core.onShowFlags((Player) sender, null);
+			return true;
+		} 
+		else if (len == 0){
+			if (!Utils.checkPlayer(sender)) return true;
+			if (Utils.hasPermission(sender, "simplyvanish.flags.display.self")) core.onShowFlags((Player) sender, null);
+			else sender.sendMessage(SimplyVanish.msgLabel+ChatColor.RED+"You do not have permission to display flags.");
+			return true;
+		} 
+		else if (hasFlags && len==1){
+			core.setFlags(args[0], args, len, sender, false, true, true);
+			if (Utils.hasPermission(sender, "simplyvanish.flags.display.other")) core.onShowFlags(sender, args[0]);
+			return true;
+		}
+		else if (len==1){
+			if (Utils.hasPermission(sender, "simplyvanish.flags.display.other")) core.onShowFlags(sender, args[0]);
+			else sender.sendMessage(SimplyVanish.msgLabel+ChatColor.RED+"You do not have permission to display flags of others.");
+			return true;
+		}
+		return unrecognized(sender);
+	}
+
+	private boolean reappearCommand(CommandSender sender, String[] args, int len,
+			boolean hasFlags) {
+		if ( len==0 ){
+			if ( !Utils.checkPlayer(sender)) return true;
+			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.self") && !Utils.checkPerm(sender, "simplyvanish.reappear.self")) return true;
+			// Let the player be seen...
+			if (hasFlags) core.setFlags(((Player) sender).getName(), args, len, sender, false, false, false);
+			if (!SimplyVanish.setVanished((Player) sender, false)) Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
+			return true;
+		} 
+		else if ( len==1 ){
+			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.other") && !Utils.checkPerm(sender, "simplyvanish.reappear.other")) return true;
+			// Make sure the other player is shown...
+			String name = args[0].trim();
+			if (hasFlags) core.setFlags(name, args, len, sender, false, true, false);
+			if (SimplyVanish.setVanished(name, false)) Utils.send(sender, SimplyVanish.msgLabel + "Show player: "+name);
+			else Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
+			return true;
+		} 
+		return unrecognized(sender);
+	}
+
+	private boolean vanishCommand(CommandSender sender, String[] args, int len,
+			boolean hasFlags) {
+		if ( len==0 ){
+			if ( !Utils.checkPlayer(sender)) return true;
+			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.self")) return true;
+			// Make sure the player is vanished...
+			if (hasFlags) core.setFlags(((Player) sender).getName(), args, len, sender, false, false, false);
+			if (!SimplyVanish.setVanished((Player) sender, true)) Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
+			return true;
+		} 
+		else if ( len==1 ){
+			if ( !Utils.checkPerm(sender, "simplyvanish.vanish.other")) return true;
+			// Make sure the other player is vanished...
+			String name = args[0].trim();
+			if (hasFlags) core.setFlags(name, args, len, sender, false, true, false);
+			if (SimplyVanish.setVanished(name, true)) Utils.send(sender, SimplyVanish.msgLabel + "Vanish player: "+name);
+			else Utils.send(sender, SimplyVanish.msgLabel+ChatColor.RED+"Action was prevented by hooks.");
+			return true;
+		} 
+		return unrecognized(sender);
+	}
+
+	/**
+	 * No flags.
+	 * @param sender
+	 * @param command
+	 * @param label Expected to be mapped already.
+	 * @param args
+	 * @return
+	 */
+	private boolean rootCommand(CommandSender sender, String[] args) {
+		SimplyVanish plugin = core.getPlugin();
+		int len = args.length;
+		if (len==1 && args[0].equalsIgnoreCase("reload")){
+			if ( !Utils.checkPerm(sender, "simplyvanish.reload")) return true;
+			plugin.loadSettings();
+			Utils.send(sender, SimplyVanish.msgLabel + ChatColor.YELLOW+"Settings reloaded.");
+			return true;
+		}
+		else if (len==1 && args[0].equalsIgnoreCase("drop")){
+			if ( !Utils.checkPerm(sender, "simplyvanish.cmd.drop")) return true;
+			if (!Utils.checkPlayer(sender)) return true;
+			Utils.dropItemInHand((Player) sender);
+			return true;
+		}
+		else if (len==1 && args[0].equals(SimplyVanish.cmdNoOpArg)) return true;
+		else if (len==1 && args[0].equalsIgnoreCase("stats")){
+			if ( !Utils.checkPerm(sender, "simplyvanish.stats.display")) return true;
+			Utils.send(sender, SimplyVanish.stats.getStatsStr(true));
+			return true;
+		} 
+		else if (len==2 && args[0].equalsIgnoreCase("stats") && args[1].equalsIgnoreCase("reset")){
+			if ( !Utils.checkPerm(sender, "simplyvanish.stats.reset")) return true;
+			SimplyVanish.stats.clear();
+			Utils.send(sender, SimplyVanish.msgLabel+"Stats reset.");
+			return true;
+		}
+		else if (len == 1 && args[0].equalsIgnoreCase("flags")){
+			if (!Utils.hasPermission(sender, "simplyvanish.flags.display.self") && !Utils.hasPermission(sender, "simplyvanish.flags.display.other")){
+				sender.sendMessage(SimplyVanish.msgLabel+ChatColor.DARK_RED+"You don't have permission to see flag info.");
+			}
+			VanishConfig cfg = new VanishConfig();
+			StringBuilder b = new StringBuilder();
+			for (Flag flag : cfg.getAllFlags()){
+				b.append(" "+Flag.fs(flag.preset)+flag.name);
+			}
+			Utils.send(sender, SimplyVanish.msgLabel + ChatColor.GRAY+"All default flags: "+ChatColor.YELLOW+b.toString());
+			return true;
+		}
+		return unrecognized(sender);
+		
+	}
+
+	/**
+	 * Message and return false.
+	 * @param sender
+	 * @return
+	 */
+	public static boolean unrecognized(CommandSender sender) {
 		Utils.send(sender, SimplyVanish.msgLabel + ChatColor.DARK_RED+"Unrecognized command or number of arguments.");
 		return false;
 	}
