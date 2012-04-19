638dbd304a6719d7793a6f13b08974e770cefd93
asofold <asofold@web.de>
2012-04-19 05:40:40 +0200

    rudimentary damage flag: damage others, shoot projectiles.

--- SimplyVanish/src/me/asofold/simplyvanish/listeners/AttackListener.java
+++ SimplyVanish/src/me/asofold/simplyvanish/listeners/AttackListener.java
@@ -10,6 +10,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 public class AttackListener implements Listener {
 	private final SimplyVanishCore core;
@@ -17,19 +19,36 @@ public class AttackListener implements Listener {
 		this.core = core;
 	}
 	
+	private final boolean shouldCancel(final String name) {
+		final VanishConfig cfg = core.getVanishConfig(name, false);
+		if (cfg == null) return false;
+		if (!cfg.vanished.state || cfg.attack.state) return false;
+		return true;
+	}
+	
 	@EventHandler(priority=EventPriority.LOW)
 	void onEntitiyDamage(EntityDamageByEntityEvent event){
 		if (event.isCancelled()) return;
 		Entity entity = event.getDamager();
 		if (entity == null) return;
-		if (entity instanceof Projectile){
-			Projectile p = (Projectile) entity;
-			entity = p.getShooter();
-		} 
+		if (entity instanceof Projectile) entity = ((Projectile) entity).getShooter();
 		if (!(entity instanceof Player)) return;
-		final VanishConfig cfg = core.getVanishConfig(((Player) entity).getName(), false);
-		if (cfg == null) return;
-		if (!cfg.vanished.state || cfg.attack.state) return;
-		event.setCancelled(true);
+		if (shouldCancel(((Player) entity).getName())) event.setCancelled(true);		
 	}
+	
+	@EventHandler(priority=EventPriority.LOW)
+	void onProjectileLaunch(ProjectileLaunchEvent event){
+		Entity entity = event.getEntity().getShooter();
+		if (!(entity instanceof Player)) return;
+		if (shouldCancel(((Player) entity).getName())) event.setCancelled(true);
+	}
+	
+	@EventHandler(priority=EventPriority.LOW)
+	void onShootBow(EntityShootBowEvent event){
+		// nmot sure about this one.
+		Entity entity = event.getEntity();
+		if (!(entity instanceof Player)) return;
+		if (shouldCancel(((Player) entity).getName())) event.setCancelled(true);
+	}
+	
 }
