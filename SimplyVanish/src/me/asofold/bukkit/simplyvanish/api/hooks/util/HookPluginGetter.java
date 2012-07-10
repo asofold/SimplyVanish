f25c55c3e2ae544257d727f7e37f47d6eaa6f095
asofold <asofold@web.de>
2012-07-10 21:55:21 +0200

    Convenience: HookPluginGetter

---
+++ SimplyVanish/src/me/asofold/bukkit/simplyvanish/api/hooks/util/HookPluginGetter.java
@@ -0,0 +1,18 @@
+package me.asofold.bukkit.simplyvanish.api.hooks.util;
+
+import me.asofold.bukkit.simplyvanish.api.hooks.HookListener;
+
+import org.bukkit.plugin.Plugin;
+
+public class HookPluginGetter<T extends Plugin> extends PluginGetter<T> implements HookListener{
+
+	public HookPluginGetter(String pluginName) {
+		super(pluginName);
+	}
+
+	@Override
+	public boolean unregisterEvents() {
+		// Override once it exists in bukkit.
+		return false;
+	}
+}
